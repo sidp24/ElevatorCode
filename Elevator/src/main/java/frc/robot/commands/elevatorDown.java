7beb372f90fcae0191fe22a6d90ea80f625696f3
Sid <sid2paul24@gmail.com>
2021-12-16 12:40:11 -0500

    Update elevatorDown.java

--- Elevator/src/main/java/frc/robot/commands/elevatorDown.java
+++ Elevator/src/main/java/frc/robot/commands/elevatorDown.java
@@ -2,18 +2,23 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 import frc.robot.Robot;
+import frc.robot.subsystems.Elevator;
 
-public class elevatorDown extends CommandGroupBase{
+public class elevatorDown extends CommandBase {
     public elevatorDown() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
         requires(Robot.m_elev);
-      }
-    
-      // Called just before this Command runs the first time
-      //changed to public
+    }
+
+    private void requires(Elevator m_elev) {
+    }
+
+    // Called just before this Command runs the first time
+      //sid - changed to public
       @Override
       public void initialize() {
 
@@ -21,26 +26,27 @@ public class elevatorDown extends CommandGroupBase{
     
       // Called repeatedly when this Command is scheduled to run
       @Override
-      protected void execute() {
+      public void execute() {
         Robot.m_elev.lower();
       }
     
       // Make this return true when this Command no longer needs to run execute()
       @Override
-      protected boolean isFinished() {
+      public boolean isFinished() {
         return !Robot.m_oi.elevDownCheck();
       }
     
       // Called once after isFinished returns true
-      @Override
-      protected void end() {
+//@Override
+      public void end() {
         Robot.m_elev.stop();
       }
     
       // Called when another command which requires one or more of the same
       // subsystems is scheduled to run
-      @Override
-      protected void interrupted() {
+      
+      // @Override
+      public void interrupted() {
         end();
       }
 }
