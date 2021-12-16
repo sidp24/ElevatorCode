9e7ed8570636f6039169f70fab1a2896377a81cd
Sid <sid2paul24@gmail.com>
2021-12-16 10:40:37 -0500

    fixed elevator

--- Elevator/src/main/java/frc/robot/subsystems/Elevator.java
+++ Elevator/src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,5 +1,40 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package frc.robot.subsystems;
 
-public class Elevator {
-    
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * Add your docs here.
+ */
+public class Elevator extends SubsystemBase {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  private final SpeedController eleMotor = new PWMVictorSPX(1);
+
+  
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public void raise() {
+    eleMotor.set(0.4);
+  }
+
+  public void lower() {
+    eleMotor.set(-0.4);
+  }
+  
+  public void stop(){
+    eleMotor.set(0);
+  }
 }
