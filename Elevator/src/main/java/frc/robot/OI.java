ac8a04f11bb72202e7b9fdb9819b139390318856
Sid <sid2paul24@gmail.com>
2021-12-15 23:34:29 -0500

    Update OI.java

--- Elevator/src/main/java/frc/robot/OI.java
+++ Elevator/src/main/java/frc/robot/OI.java
@@ -1,5 +1,27 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.*;
+
 public class OI {
-    
+    private final Joystick logi = new Joystick(0);
+    private final Button btnUp;
+    private final Button btnDown;
+
+    public OI() {
+        btnUp = new JoystickButton(logi, 3);
+        btnDown = new JoystickButton(logi, 4);
+        btnUp.whileHeld(new elevatorUp());
+        btnDown.whileHeld(new elevatorDown());
+    }
+
+    public boolean elevUpCheck() {
+        return btnUp.get();
+    }
+
+    public boolean elevDownCheck() {
+        return btnDown.get();
+    }
 }
